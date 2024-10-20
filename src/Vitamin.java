4c1ed24faffb4cef8483ff714623f0c8a94e7f91
AleksandarGajic0 <aleksandar2608@hotmail.com>
2024-10-20 16:36:53 -0400

    Create Vitamin.java

---
+++ src/Vitamin.java
@@ -0,0 +1,15 @@
+package week12_encapsulation
+
+public class Vitamin {
+
+  private String name;
+
+  public Vitamin(String name){
+
+    this.name = name;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+}
